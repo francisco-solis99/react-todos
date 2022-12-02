5dd54c84a9d147721111fe657cdb37ecb591742d
francisco-solis99 <franciscosoliscss@gmail.com>
2022-12-01 19:18:49 -0600

    [Add] some animations

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,21 +3,26 @@ import { TodosContext } from '../context/TodosContext';
 import Checkmark from './Checkmark';
 import PropTypes from 'prop-types';
 import '../css/todo.css';
-
-
 import { MdDelete } from 'react-icons/md';
 
 function Todo({ todo }) {
   const { done, text, id, visible } = todo;
   const { deleteTodo, changeDoneTodo } = useContext(TodosContext);
 
-  const handlerDeleteTodo = () => deleteTodo(id);
+  const handlerDeleteTodo = (e) => {
+    const todoToDelete = e.currentTarget.parentElement;
+    todoToDelete.classList.add('to-delete');
+    setTimeout(() => {
+      console.log('Delete...');
+      deleteTodo(id);
+    }, 0);
+  };
 
   return (
     <li className={`list-item  ${done ? 'done' : ''} ${!visible ? 'no-visible' : ''}`}>
       <span>{text}</span>
       <Checkmark done={done} id={id} onClickCheckmark={changeDoneTodo} />
-      <button className="Todo__button delete" onClick={handlerDeleteTodo}>
+      <button className="Todo__button delete" onClick={(e) => handlerDeleteTodo(e)}>
         <MdDelete />
       </button>
     </li>
