7227d18175172224a888ef0cc660b3f3781072af
francisco-solis99 <franciscosoliscss@gmail.com>
2022-12-01 19:19:49 -0600

    [Refactor] useRef instead of useState for value

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,36 +1,51 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useRef } from 'react';
 import { TodosContext } from '../context/TodosContext';
 import '../css/form.css';
+import ToolTip from '../components/ToolTip';
 
 
 const initState = '';
 
 function Form() {
-  const [value, setValue] = useState(initState);
-
+  const valueRef = useRef();
+  const [viewTooolTip, setViewToolTip] = useState(false);
+  const [msg, setMsg] = useState('');
   const { createTodo } = useContext(TodosContext);
+
+  const launchToolTip = (text) => {
+    setViewToolTip(true);
+    setMsg(text);
+    setTimeout(() => {
+      setMsg('');
+      setViewToolTip(false)
+    }, 3000);
+  }
 
   const handlerClickSubmit = (e) => {
     e.preventDefault();
-    if (value !== initState) {
-      createTodo({ text: value, done: false });
-      setValue(initState);
+    if (valueRef.current.value !== initState) {
+      createTodo({ text: valueRef.current.value, done: false });
+      valueRef.current.value = '';
+      launchToolTip('Task added');
+      return;
     }
+    launchToolTip('Invalid task');
   }
 
 
   return (
-    <form className='Form' onSubmit={(e) => handlerClickSubmit(e)} >
-      <input type='text'
-        className='Form__input'
-        placeholder='Agrega una tarea'
-        value={value}
-        onChange={e => {
-          setValue(e.currentTarget.value);
-        }}
-      />
-      <button className='Form__button' type='submit'>Send</button>
-    </form>
+    <>
+      <ToolTip message={msg} show={viewTooolTip} />
+      <form className='Form' onSubmit={(e) => handlerClickSubmit(e)} >
+        <input type='text'
+          className='Form__input'
+          placeholder='Add a task'
+          ref={valueRef}
+        />
+        <button className='Form__button' type='submit'>Create</button>
+      </form>
+    </>
+
   )
 };
 
